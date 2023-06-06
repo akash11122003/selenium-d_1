788d9c928406a8640e59d0fdb516ae85636336be
akash11122003 <97861184+akash11122003@users.noreply.github.com>
2023-06-06 09:22:11 +0530

    Add files via upload

---
+++ Day 8/exercise4.java
@@ -0,0 +1,28 @@
+package com.day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class exercise4 {
+	public static void main(String [] args)
+	{
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://www.abhibus.com/bus-ticket-booking");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"source\"]")).sendKeys("Coimbatore");
+		driver.findElement(By.xpath("//*[@id=\"destination\"]")).sendKeys("Varanasi");
+		WebElement date=driver.findElement(By.xpath("//*[@id=\"datepicker1\"]"));
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('value','11/12/2023')", date);
+		
+		
+		
+	}
+
+}
